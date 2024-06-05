e8564d9c9f4960ac6a910f1739189254de364ffc
Calum Moir-Mattox <calummm@gmail.com>
2024-06-05 14:39:47 +1000

    add miryoku definitions

---
+++ manna-harbour_miryoku/custom_rules.mk
@@ -0,0 +1,9 @@
+# Copyright 2019 Manna Harbour
+# https://github.com/manna-harbour/miryoku
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+WPM_ENABLE = yes
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
